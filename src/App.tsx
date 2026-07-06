6d50868ad37caf6a05c5568d315c5783402e925d
Drei08 <andrei_rheivjarth@hotmail.com>
2026-07-06 20:05:29 -0300

    criado-header-footer

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,27 @@
 import React from 'react';
+import Header from './components/Header';
+import Footer from './components/Footer';
 
+//CSS
+import styles from './App.module.css';
 
 
 function App() {
   return (
     <div>
-      <h1>Anotacoes de Tarefas</h1>
+      <Header />
+      <main className={styles.main}>
+        <h1>Bem-vindo ao Anotações Tarefas!</h1>
+        <div>
+          <h2>O que você vai fazer hoje?</h2>
+          <p>Formulário</p>
+        </div>
+        <div>
+          <h2>Suas Tarefas:</h2>
+          <p>Lista</p>
+        </div>
+      </main>
+      <Footer />
     </div>
   );
 }
---
+++ src/components/Footer.tsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+//CSS
+import styles from './Footer.module.css'
+
+
+const Footer = () => {
+  return (
+    <footer className={styles.footer}>
+      <p>
+         <span>Anotações Tarefas</span> @ 2026
+      </p>
+    </footer>
+  )
+}
+
+export default Footer
